4fbc71fff8f7ab87da9e6242841ec2925d81f2dc
Gyanesh Rao <gyaneshrao28@gmail.com>
2024-06-21 12:24:29 +0530

    v-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-          <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-            <Navbar />
-            {children}
-          </main> 
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white bg-[#0d1224]">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   );
