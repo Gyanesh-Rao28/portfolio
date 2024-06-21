297fdaaaaf1bc38f9860fccc70e717ff3d0e1781
Gyanesh Rao <gyaneshrao28@gmail.com>
2024-06-21 12:17:16 +0530

    v-1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "images.unsplash.com",
+            "utfs.io",
+            "tailwindui.com",
+            "cdn.discordapp.com",
+            "picsum.photos"
+        ]
+    }
+};
 
 export default nextConfig;
---
+++ utils/data/personal-data.ts
@@ -0,0 +1,14 @@
+export const personalData = {
+    name: "S Gyanesh Rao",
+    profile: '/profile.png',
+    designation: "Software Developer",
+    description: "My name is S Gyanesh Rao. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    email: 'gyaneshrao28@gmail.com',
+    phone: '+91-8815572510',
+    address: 'Maruti Residency Amlidih, Raipur, Chhattisgarh',
+    github: 'https://github.com/Gyanesh-Rao28',
+    linkedIn: 'https://www.linkedin.com/in/gyanesh-rao-2b3927222/',
+    leetcode: "https://leetcode.com/u/gyaneshrao28/",
+    devUsername: "said7388",
+    resume: "https://drive.google.com/file/d/10jhK9uoipxcbO1xyiD4GYxLjrkcxOlCL/view?usp=sharing"
+}
