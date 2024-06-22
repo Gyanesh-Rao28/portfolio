46a783b4275a3ca657ea94584b2ce6a39c89c4d3
Gyanesh Rao <gyaneshrao28@gmail.com>
2024-06-22 15:35:55 +0530

    v-1.3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,25 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
-    // rest of the code
+    extends:{
+      animation:{
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes:{
+        scroll:{
+          to:{
+            transform: "translate(calc(-50% - 0.5rem))",
+
+          }
+        }
+      }
+    }
   },
   plugins: [
-    // rest of the code
     addVariablesForColors,
     require('daisyui'),
   ],
--- utils/data/experience-data.ts
+++ utils/data/experience-data.ts
@@ -5,20 +5,22 @@ import { LuGraduationCap } from "react-icons/lu";
 
 export const experiencesData = [
     {
-        title: "Graduated bootcamp",
-        location: "Miami, FL",
+        title: "E-sports Manager",
+        location: "SRM University",
         description:
             "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
         icon: React.createElement(LuGraduationCap),
-        date: "2019",
+        date: "2021-2024",
+        algin:"timeline-start"
     },
     {
-        title: "Front-End Developer",
+        title: "Front-End Developer NTL",
         location: "Orlando, FL",
         description:
             "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
         icon: React.createElement(CgWorkAlt),
         date: "2019 - 2021",
+        algin: "timeline-end"
     },
     {
         title: "Full-Stack Developer",
@@ -27,5 +29,6 @@ export const experiencesData = [
             "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
         icon: React.createElement(FaReact),
         date: "2021 - present",
+        algin: "timeline-start"
     },
 ] as const;
