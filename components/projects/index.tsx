359ea94518b68abf5487513ad067b24410ae32a7
Gyanesh Rao <gyaneshrao28@gmail.com>
2024-06-25 10:36:30 +0530

    v-1.4

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,10 +1,8 @@
 "use client";
-import Image from "next/image";
+
 import SectionTitle from "../sectionTitle";
-
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
-
-import { SiGithub } from "react-icons/si";
+import { ProjectsData } from "@/utils/data/project-data";
 
 const Projects = () => {
   return (
@@ -12,7 +10,7 @@ const Projects = () => {
       <SectionTitle title="Projects" />
       <div className="my-20">
         <BentoGrid className="max-w-full mx-auto">
-          {items.map((item, i) => (
+          {ProjectsData.map((item, i) => (
             <BentoGridItem
               key={i}
               title={item.title}
@@ -30,106 +28,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
-
-const items = [
-  {
-    title: "Smart Verify",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <SiGithub className="h-5 w-5 text-neutral-500" />,
-    tools: [
-      {
-        id: 1,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 2,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 3,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 4,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-    ],
-  },
-  {
-    title: "Petify",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <SiGithub className="h-5 w-5 text-neutral-500" />,
-    tools: [
-      {
-        id: 1,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 2,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 3,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 4,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-    ],
-  },
-  {
-    title: "CPP + MySql",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <SiGithub className="h-5 w-5 text-neutral-500" />,
-    tools: [
-      {
-        id: 1,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 2,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 3,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-      {
-        id: 4,
-        name: "John Doe",
-        image:
-          "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-      },
-    ],
-  },
-];  
---
+++ utils/data/project-data.tsx
@@ -0,0 +1,105 @@
+import { SiGithub } from "react-icons/si";
+
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    
+  </div>
+);
+export const ProjectsData = [
+    {
+        title: "Smart Verify",
+        description: "Explore the birth of groundbreaking ideas and inventions.",
+        header: <Skeleton />,
+        icon: <SiGithub className="h-5 w-5 text-neutral-500" />,
+        tools: [
+            {
+                id: 1,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 2,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 3,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 4,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+        ],
+    },
+    {
+        title: "Petify",
+        description: "Explore the birth of groundbreaking ideas and inventions.",
+        header: <Skeleton />,
+        icon: <SiGithub className="h-5 w-5 text-neutral-500" />,
+        tools: [
+            {
+                id: 1,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 2,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 3,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 4,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+        ],
+    },
+    {
+        title: "CPP + MySql",
+        description: "Explore the birth of groundbreaking ideas and inventions.",
+        header: <Skeleton />,
+        icon: <SiGithub className="h-5 w-5 text-neutral-500" />,
+        tools: [
+            {
+                id: 1,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 2,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 3,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+            {
+                id: 4,
+                name: "John Doe",
+                image:
+                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            },
+        ],
+    },
+]; 
